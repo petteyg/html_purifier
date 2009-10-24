f1164c737a6601f12827734e1471b434e95ed80d
Gordon Pettey <petteyg359@gmail.com>
2009-10-24 19:04:15 -0400

    Add Component

---
+++ controllers/components/html_purifier.php
@@ -0,0 +1,35 @@
+<?php
+App::import('Vendor', 'HtmlPurifier.HTMLPurifier', array('file' => 'htmlpurifier/HTMLPurifier.standalone.php'));
+
+class HtmlPurifierComponent extends Object {
+
+	var $config = array(
+		'Core.Encoding' => 'utf-8',
+		'HTML.DefinitionID' => 'CakePHP',
+		'HTML.DefinitionRev' => 1,
+		'HTML.TidyLevel' => 'medium',
+		'HTML.Doctype' => 'XHTML 1.0 Transitional'
+	);
+
+	var $Purifier;
+
+	var $PurifierConfig;
+
+	function initialize(&$controller, $settings = array()) {
+		$this->config = array_diff($this->config, $settings);
+		foreach ($settings as $setting => $value) {
+			$this->config[$setting] = $value;
+		}
+	}
+
+	function startup(&$controller) {
+		$this->PurifierConfig = HTMLPurifier_Config::createDefault();
+		$this->Purifier =& new HTMLPurifier($this->PurifierConfig);
+	}
+
+	function purify($data) {
+		return $this->Purifier->purify($data);
+	}
+
+}
+?>
